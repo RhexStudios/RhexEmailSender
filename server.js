bcfd1679bc8a7a21fa4876f4f6b840edccc81670
henriquegouveia1 <henriquegs002@gmail.com>
2022-11-26 11:48:23 -0300

    update/updating server and creating script start
    update/updating server and creating script start

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-
 const express = require('express');
 const app = express();
 const port = '8000';
 const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
+    port:465,
     service: 'gmail',
     auth: {
         user: 'henriquegs002@gmail.com',
         pass: 'nezlmrvivdnpvggl'
-    }
+    },
+    secure: true
 });
 
-var mailOptions = {
-    from: 'henriquegs002@gmail.com',
-    to: 'ickbola@gmail.com',
-    subject: 'teste',
-    text: 'mais facil que parece',
-    html: ''
-};
+route.post('/text-mail', (req,res)=>{
 
-transporter.sendMail(mailOptions, function (err, info) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(info.response)
-    }
-})
+    var mailOptions = {
+        from: 'henriquegs002@gmail.com',
+        to: 'ickbola@gmail.com',
+        subject: 'teste',
+        text: 'mais facil que parece',
+        html: ''
+    };
+    
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(info.response)
+        }
+    })
 
+});
 
-
-app.listen(port)
+app.listen(port);
 
