f2e352f088d2c0c9528b448d0ad7f3555d4eeecd
Lauro de Brito Vieira Filho <laurofilho.cq1@wizsolucoes.com.br>
2022-11-30 03:26:29 -0300

    post e get de email

--- Infra/EmailRepository.js
+++ Infra/EmailRepository.js
@@ -1,16 +1,33 @@
 const { Sequelize } = require('sequelize');
 const email = require('../Domain/Models/EmailModel');
+const { db } = require('./Database');
+const { express, Request, Response } = require('express');
 
 //#region - Post -
-
-
+const postEmail = async (req, res) => {
+  try {
+    const mail = req.body;
+    await email.create(
+      mail
+    )
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+}
 //#endregion
 
-
-
 //#region - Gets - 
-const findEmails = await Email.findAll({ limit: 10 });
-console.log(Email.every(email => email instanceof Emails));
-console.log('Todos os Emails:', JSON.stringify(emails, null, 2));
+const findEmails = async ( req, res) => {
+  try {
+    const { query } = req.params;
+    const emails = await Email.findAll({ limit: 10 });
+    res.send.json( emails );
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+}
 
 //#endregion
